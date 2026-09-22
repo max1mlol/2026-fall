b1459905c7f5d21549fb8c574b470c0af370b229
Munkhjargal Manlaibayar <mxmmunkhjargal@gmail.com>
2026-09-23 02:42:15 +0800

    event

--- Software-Architecture/lab1/oop-typescript/src/events.ts
+++ Software-Architecture/lab1/oop-typescript/src/events.ts
@@ -7,13 +7,17 @@ const bus = new CartEvents();
 // Хэрэглэх
 bus.on('ITEM_ADDED', (it: Item) => console.log('Нэмэгдсэн:', it.name));
 bus.on('CHECKOUT', (total: number) => console.log('Нийт checkout:', total));
-
+bus.on('ITEM_REMOVED', (it: Item) => console.log('Хасагдсан: ', it.name));
 // энгийн cart “модуль”
 let items: Item[] = [...cart];
 
 function addItem(it: Item) {
     items = [...items, it];
     bus.emit('ITEM_ADDED', it);
+}
+function removeItem(it: Item) {
+    items = items.filter(i => i !== it);
+    bus.emit('ITEM_REMOVED', it);
 }
 function checkout() {
     const total = items.reduce((s, i) => s + i.price * i.qty, 0);
@@ -22,4 +26,5 @@ function checkout() {
 }
 // ажиллуулах
 addItem({ name: 'Chocolate', price: 4200, qty: 3, category: 'food' });
+removeItem({ name: 'Chocolate', price: 4200, qty: 3, category: 'food' });
 checkout();
